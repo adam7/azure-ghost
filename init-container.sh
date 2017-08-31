622f0045d2354c21c6bd1c8c1502df75b8fb21d4
Adam Cooper <adam@sequence7.net>
2017-08-31 21:08:26 +0200

    Switch to sqlite

--- init-container.sh
+++ init-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gosu node ghost config --db="mysql" --dbhost="$DB_HOST" --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --url="$GHOST_URL"
+gosu node ghost config --db="sqlite3" --dbpath="$GHOST_CONTENT/data/ghost.db"
 gosu node ghost config paths.contentPath "$GHOST_CONTENT"
 gosu node ghost config server.host 0.0.0.0
 sh /bin/migrate_util.sh
